3085c2e27c0e60878044128d5d323ee7e4150899
Neox63 <matchambon60@gmail.com>
2021-01-18 01:57:58 +0100

    Typo change

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
 
     return (
         <div style={{ backgroundColor: '#252526' }} className="text-center p-3 mt-5">
-            <h4>Created with <i style={{ color: 'red' }} className="fas fa-heart"></i> by <a style={{ color: '#569CD6' }} href="#about">me</a> !</h4>
+            <h5>Created with <i style={{ color: 'red' }} className="fas fa-heart"></i> by <a style={{ color: '#569CD6' }} href="#about">me</a> !</h5>
         </div>
     )
 }
