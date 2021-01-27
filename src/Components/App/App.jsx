6a385069aec154d6f48289541df1173eca90a539
Neox63 <matchambon60@gmail.com>
2021-01-27 20:28:55 +0100

    Removed animation

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -10,52 +10,26 @@ import Footer from '../Footer/Footer';
 
 const App = () => {
 
-  const [vantaEffect, setVantaEffect] = useState(0);
-  const myRef = useRef(null);
-
-  useEffect(() => {
-    if (!vantaEffect) {
-      setVantaEffect(NET({
-        el: myRef.current,
-        backgroundColor: "#1E1E1E",
-        scale: 1.00,
-        scaleMobile: 1.00,
-        mouseControls: false,
-        touchControls: false,
-        gyroControls: false,
-        color: 0x7787,
-        maxDistance: 15.00,
-        spacing: 15.00,
-      }))
-    }
-
-    return () => {
-      if (vantaEffect) vantaEffect.destroy()
-    }
-  }, [vantaEffect])
-
   return (
       <Fragment>
-        <div ref={myRef}>
-          <div id="landing">
-            <Hero />
-          </div>
-          
-          <div id="about">
-            <AboutMe />
-          </div>
+        <div id="landing">
+          <Hero />
+        </div>
+        
+        <div id="about">
+          <AboutMe />
+        </div>
 
-          <div id="skills">
-          <Skills />
-          </div>
+        <div id="skills">
+        <Skills />
+        </div>
 
-          <div id="projects">
-            <Projects />
-          </div>
+        <div id="projects">
+          <Projects />
+        </div>
 
-          <div id="footer">
-            <Footer />
-          </div>
+        <div id="footer">
+          <Footer />
         </div>
       </Fragment>
   );
